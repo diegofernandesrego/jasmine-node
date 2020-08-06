b74326c0b7eed023b3f41ca79fd77b11a3bc336a
C1310640 Diego Fernandes do Rego <c1310640@interno.bb.com.br>
2020-08-05 23:52:28 -0300

    Arquivos do projeto local

---
+++ spec/support/exemploSpec.js
@@ -0,0 +1,11 @@
+describe("Suite de teste de exemplo com NodeJs", function(){
+    
+    
+    it("deve demonstrar o uso do Jasmine com NodeaJs", function(){
+        expect(true).toEqual(true);
+    });
+
+    it("deve demostrar o uso do karma em modo de escuta", function(){
+        expect(1).toEqual(1);
+    });
+})
